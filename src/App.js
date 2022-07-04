9c40fad7b86d810dbf85424ded9121b015a56b07
unknown <mohi10503@gmail.com>
2022-07-04 21:24:56 +0530

    update our menu-items to navigate with Router HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 
+// import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
+
+
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+        </Routes>
+      </BrowserRouter> */}
+
+
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+
+      {/* <HomePage/> */}
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,37 +5,42 @@ import './directory.styles.scss';
 class Directory extends React.Component {
     constructor(){
         super();
-
         this.state = {
             sections: [
                 {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1
+                  id: 1,
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2
+                  id: 2,
+                  linkUrl: ''
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3
+                  id: 3,
+                  linkUrl: ''
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://images.pexels.com/photos/2229490/pexels-photo-2229490.jpeg?auto=compress&cs=tinysrgb&w=1600',
                   // 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
-                  id: 4
+                  id: 4,
+                  linkUrl: ''
+
                 },
                 {
                   title: 'mens',
                   imageUrl: 'https://images.pexels.com/photos/715546/pexels-photo-715546.jpeg?auto=compress&cs=tinysrgb&w=1600',
                   // 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
-                  id: 5
+                  id: 5,
+                  linkUrl: ''
                 }
               ]
         }
@@ -44,10 +49,12 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    // title, imageUrl, id, size, linkUrl
+                    // title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} history={this.props.history}
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
-                }
+                } 
             </div>
         );
     }
